202928c69905b5a4d5f87423dd89bfc949103a5a
Hoaper <53962920+Hoaper@users.noreply.github.com>
2021-08-25 17:53:52 +0000

    Update t_bot.py

--- t_bot.py
+++ t_bot.py
@@ -3,26 +3,25 @@ import os
 from subprocess import Popen, PIPE
 
 TOKEN = os.environ.get("TGM_TOKEN")
-
-# TELEGRAM CVARS
-
-
+secret_key = os.environ.get("TGM_KEY")
 bot = telebot.TeleBot(TOKEN)
 
 print(f"Telegram connected successfully!")
-global handled
-
-handled = False
 
 @bot.message_handler(commands=['handle'])
 def handle(message):
-	if not handled:
-		channel = str(message.chat.id)
-		print(f'[+] {message.chat.id}')
-		bot.send_message(channel, "Handler started")
-	
-		handled = True
-		handling(channel)
+	try:
+		msg_arg = message.text.split(" ")[1]
+		if msg_arg == secret_key
+			channel = str(message.chat.id)
+			print(f'[+] {message.chat.id}')
+			bot.send_message(channel, "Handler started")
+
+			handling(channel)
+		
+	except Exception:
+		
+		bot.send_message(message.chat.id, "Auth failure")
 
 
 @bot.message_handler(commands=["print"])
