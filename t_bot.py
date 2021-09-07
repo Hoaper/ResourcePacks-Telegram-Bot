09d902a07fe9ee6c57b22405747d81553ee8d1c4
Hoaper <53962920+Hoaper@users.noreply.github.com>
2021-09-07 15:51:09 +0000

    Update t_bot.py

--- t_bot.py
+++ t_bot.py
@@ -4,12 +4,14 @@ import asyncio
 from t_mail import Mail
 
 TOKEN = os.environ.get("TGM_TOKEN")
+OWNER_ID = 1911939737
+OWNER_ID = os.environ.get("OWNER_ID")
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle(message):
 	try:
-		if message.from_user.id == 1911939737:
+		if message.from_user.id == OWNER_ID:
 			channel = str(message.chat.id)
 			print(f'[+] {message.chat.id}')
 			bot.send_message(channel, "Handler started")
